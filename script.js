cdf9acf85333ad2e4b94a6fffe6ad694feb9100c
Parthiv Gajjar <pugajjar4706@gmail.com>
2026-03-11 19:21:33 +0530

    Refactor Supabase client and error handling
    Updated Supabase key and modified error handling to use alerts.

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
 const SUPABASE_URL = "https://mvjbtktibnvrzsszfwjo.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12amJ0a3RpYm52cnpzc3pmd2pvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzYyNzQsImV4cCI6MjA4ODgxMjI3NH0.kJIRwd1dA0Z6JLuw44zIWMV_GWKuKxaCmCf42oTYfuA";
+const SUPABASE_KEY = "sb_publishable_xZZFF2j5L8dWgkj_nlnUlA_pHJv_eNs";
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 
-document.getElementById("studentForm").addEventListener("submit", async function(e){
+document.getElementById("studentForm").addEventListener("submit", async function(e) {
 
-e.preventDefault();
+  e.preventDefault();
 
-const name = document.getElementById("name").value;
-const email = document.getElementById("email").value;
-const enrollment = document.getElementById("enrollment").value;
-const branch = document.getElementById("branch").value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const enrollment = document.getElementById("enrollment").value;
+  const branch = document.getElementById("branch").value;
 
-const { data, error } = await supabaseClient
-.from("students")
-.insert([
-{ name, email, enrollment, branch }
-]);
+  const { data, error } = await supabaseClient
+  .from("students")
+  .insert([
+    { name, email, enrollment, branch }
+  ]);
 
-if(error){
-
-document.getElementById("message").innerText = "Error: " + error.message;
-
-}else{
-
-document.getElementById("message").innerText = "Data inserted successfully!";
-document.getElementById("studentForm").reset();
-
-}
-
+  if(error){
+    console.log(error);
+    alert("Error inserting data");
+  }else{
+    alert("Data inserted successfully!");
+  }
 
 });
-
